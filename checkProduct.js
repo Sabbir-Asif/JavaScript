9e59719400d5c5b89c25572616649bd68517f517
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-07-23 16:35:44 +0600

    commit

--- checkProduct.js
+++ checkProduct.js
@@ -11,3 +11,21 @@ if(whatToDo === "2")
     console.log("The product available : " + myProducts);
     document.write("<p class = info> The products available : " + myProducts + "  </p>");
 }
+else if(whatToDo === "1")
+{
+    let product = prompt("Enter the name of the product to see if availbale : ");
+    if(myProducts.indexOf(product) === -1)
+    {
+        console.log("Sorry the product is not available");
+        document.write("<p class = error>Sorry the product is not available</p>");
+    }
+    else
+    {
+        console.log("The product is available");
+        document.write("<p class = success>The product is available</p>")
+    }
+}
+else
+{
+    document.write("<p class = info>Refresh the page if you want to look for a product or list of all product</p>");
+}
