6906c6f5770cd9cb4091d5d39db9ff17491b34bc
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2023-07-29 11:52:53 +0600

    xyz

---
+++ forEachTest.js
@@ -0,0 +1,12 @@
+var arr = [
+    "banana",
+    "apple",
+    "orange",
+    "mango",
+    "grape"
+];
+
+arr.forEach(function(element, index)
+{
+    console.log(element + " is at " + index);
+});
